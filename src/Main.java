65b06054630dbf8aacf54900bc434d4a7334e844
AlfonsoTorrez <alftorres@csumb.edu>
2020-07-28 10:53:14 -0700

    refactor database connection

--- src/Main.java
+++ src/Main.java
@@ -1,43 +1,49 @@
-import java.sql.*; 
+//import java.sql.*; 
+import database.DBConnection;
 
 public class Main {
 
 	public static void main(String[] args) {
 		System.out.println("Right now I am connecting my database."); 
-		Statement stmt = null;
-		try {
-			Class.forName("com.mysql.cj.jdbc.Driver");
-			Connection conn = DriverManager.getConnection("jdbc:mysql://localhost:3306/our_notes?useSSL=false&serverTimezone=UTC#","root","Torrez11%");
-			//PreparedStatement ps = conn.prepareStatement("SELECT * FROM users;");
-			//boolean x = ps.execute(); 
-			stmt = conn.createStatement();
-			ResultSet rs = stmt.executeQuery("SELECT * FROM users;"); 
-			
-	      while(rs.next()){
-	          //Retrieve by column name
-	          int id  = rs.getInt("user_id");
-	          String uName = rs.getString("username");
-	          String pass = rs.getString("password");
-	          String fName = rs.getString("first_name");
-	          String email = rs.getString("email");
-
-	          //Display values
-	          System.out.print("ID: " + id);
-	          System.out.print(", Username: " + uName);
-	          System.out.print(", Password: " + pass);
-	          System.out.print(", First Name: " + fName);
-	          System.out.println(", Email: " + email);
-	       }
-	       rs.close();
-//			if(x) {
-//				System.out.println("Registration done successfully..."); 
-//			}else {
-//				System.out.println("Registration Failed..."); 
-//			}
-			
-		}catch(Exception e1) {
-			System.out.println(e1); 
-		}
+		DBConnection obj = new DBConnection(); 
+		obj.getRows();
+		
+//		Statement stmt = null;
+//		try {
+//			Class.forName("com.mysql.cj.jdbc.Driver");
+//			Connection conn = DriverManager
+//					.getConnection("jdbc:mysql://localhost:3306/our_notes?useSSL=false&serverTimezone=UTC#","root","Torrez11%");
+//			//Use code below to use INSERT
+//			//PreparedStatement ps = conn.prepareStatement("SELECT * FROM users;");
+//			//boolean x = ps.execute(); 
+//			stmt = conn.createStatement();
+//			ResultSet rs = stmt.executeQuery("SELECT * FROM users;"); 
+//			
+//	      while(rs.next()){
+//	          //Retrieve by column name
+//	          int id  = rs.getInt("user_id");
+//	          String uName = rs.getString("username");
+//	          String pass = rs.getString("password");
+//	          String fName = rs.getString("first_name");
+//	          String email = rs.getString("email");
+//
+//	          //Display values
+//	          System.out.print("ID: " + id);
+//	          System.out.print(", Username: " + uName);
+//	          System.out.print(", Password: " + pass);
+//	          System.out.print(", First Name: " + fName);
+//	          System.out.println(", Email: " + email);
+//	       }
+//	       rs.close();
+////			if(x) {
+////				System.out.println("Registration done successfully..."); 
+////			}else {
+////				System.out.println("Registration Failed..."); 
+////			}
+//			
+//		}catch(Exception e1) {
+//			System.out.println(e1); 
+//		}
 		
 	}
 
---
+++ src/database/DBConnection.java
@@ -0,0 +1,47 @@
+package database; 
+import java.sql.*; 
+
+
+public class DBConnection {
+	private Connection conn; 
+	private Statement stmt; 
+	private ResultSet rs; 
+	
+	public DBConnection(){
+		try{
+			Class.forName("com.mysql.cj.jdbc.Driver");
+			conn = DriverManager
+					.getConnection("jdbc:mysql://localhost:3306/our_notes?useSSL=false&serverTimezone=UTC#","root","Torrez11%");
+			stmt = conn.createStatement();
+		}catch(Exception e1) {
+			System.out.println(e1); 
+		}
+		
+	}
+	//Take query to display 
+	public void getRows() {
+		String query = "SELECT * FROM users;"; 
+		try {
+			rs = stmt.executeQuery(query); 
+	      while(rs.next()){
+	          //Retrieve by column name
+	          int id  = rs.getInt("user_id");
+	          String uName = rs.getString("username");
+	          String pass = rs.getString("password");
+	          String fName = rs.getString("first_name");
+	          String email = rs.getString("email");
+
+	          //Display values
+	          System.out.print("ID: " + id);
+	          System.out.print(", Username: " + uName);
+	          System.out.print(", Password: " + pass);
+	          System.out.print(", First Name: " + fName);
+	          System.out.println(", Email: " + email);
+	       }
+	       rs.close();
+		}catch(Exception e1) {
+			System.out.println(e1); 
+		}
+	}
+	
+}
