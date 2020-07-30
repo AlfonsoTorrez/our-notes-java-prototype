611284fd02a5e1ae746e0106c8c91d6b885472c8
AlfonsoTorrez <alftorres@csumb.edu>
2020-07-30 09:13:29 -0700

    connected mysql db

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,9 @@
-package sample;
-
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
+import database.DBConnection;
 
 public class Main extends Application {
 
@@ -13,11 +12,15 @@ public class Main extends Application {
         Parent root = FXMLLoader.load(getClass().getResource("logIn.fxml"));
         primaryStage.setTitle("Hello World");
         primaryStage.setScene(new Scene(root, 400, 300));
+        primaryStage.setResizable(false);
         primaryStage.show();
     }
 
 
     public static void main(String[] args) {
+        System.out.println("Right now I am connecting my database.");
+        DBConnection obj = new DBConnection();
+        obj.getRows();
         launch(args);
     }
 }
